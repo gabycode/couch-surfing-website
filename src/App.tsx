b74a3c6eb3c42d116084fe66265567a6793a6cb5
gabycode <86427446+gabycode@users.noreply.github.com>
2023-05-29 21:32:18 -0400

    add: ts exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const reviews = [
+  {
+    name: "Sheia",
+    stars: 5,
+    loyaltyUser: true,
+    date: "01-04-2021",
+  },
+  {
+    name: "Andrzej",
+    stars: 3,
+    loyaltyUser: false,
+    date: "28-03-2021",
+  },
+  {
+    name: "Omar",
+    stars: 4,
+    loyaltyUser: true,
+    date: "27-03-2021",
+  },
+];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const showTotalReviews = (
+  value: number,
+  reviewer: string,
+  date: string,
+  loyalty: boolean
+) => {
+  const totalReviews = value.toString();
+  const lastReviewer = reviewer;
+  const lastReviewedDate = date;
+  const isLoyalUser = loyalty ? "⭐" : "";
+  return `total reviews: ${totalReviews} and last reviewed by: ${lastReviewer} ${isLoyalUser} on ${lastReviewedDate}`;
+};
+
+const you = {
+  userName: { firstName: "Bobby", lastName: "Brown" },
+  isReturning: true,
+};
+
+function populateUser(isReturning: boolean, userName: string) {
+  if (isReturning) {
+    return "back" + userName;
+  }
+  return userName;
 }
 
-export default App
+function App() {
+  return (
+    <>
+      <div className="nav-bar">
+        <div className="logo"></div>
+        <h3 className="user-container">
+          Welcome
+          <span id="user">
+            {populateUser(you.isReturning, you.userName.firstName)}
+          </span>
+        </h3>
+      </div>
+      <div className="container">
+        <h5 id="reviews">
+          total reviews:{" "}
+          {showTotalReviews(
+            reviews.length,
+            reviews[0].name,
+            reviews[0].date,
+            reviews[0].loyaltyUser
+          )}
+        </h5>
+      </div>
+    </>
+  );
+}
+
+export default App;
