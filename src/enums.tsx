9583709a5bedd4e67e5be627c3e4fa341f0789ac
gabycode <86427446+gabycode@users.noreply.github.com>
2023-06-19 20:57:08 -0400

    create: enums

---
+++ src/enums.tsx
@@ -0,0 +1,10 @@
+export enum Permission {
+  ADMIN = "ADMIN",
+  READ_ONLY = "READ_ONLY",
+}
+
+export enum LoyaltyUser {
+  GOLD_USER = "GOLD_USER",
+  SILVER_USER = "SILVER_USER",
+  BRONZE_USER = "BRONZE_USER",
+}
